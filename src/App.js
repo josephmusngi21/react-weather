f9f5fba282cc784163202d0b266695326ffa8ac6
Josephmusngi21 <josephmusngi7@gmail.com>
2024-07-01 18:23:26 -0700

    added files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'; 
+import { createRoot } from 'react-dom/client';
+import Card from '/Card';
+
+function App() {
+
+    return(
+    <>
+        <Card />    
+    </>
+    )
+}
+
+export default App;
